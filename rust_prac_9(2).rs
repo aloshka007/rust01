40a6d33a894910374ee7362b239f3ae462f9d7ab
aloshka007 <o.grechan@e-u.edu.ua>
2024-11-18 23:28:01 +0300

    Create

---
+++ rust_prac_9(2).rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct TrafficLight {
+    color: String,
+}
+
+impl TrafficLight {
+    pub fn show_state(&self)  {
+        println!("the current state is {}", self.color); 
+    }
+}
+
+fn main() {
+    let light = TrafficLight{
+        color: "red".to_owned(),
+    };
+    
+    light.show_state(); // Borrow `light` to call the method.
+   
+    println!("{:?}", light); // We can still use `light` after borrowing.
+}
